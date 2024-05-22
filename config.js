47e2d19163d3eac27f4bd19de3aa4dde831da0ed
Moonlox <moonloxv1@gmail.com>
2024-05-22 13:40:07 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'token': 'MTI0Mjc5ODYwNDYwMzYyNTU3Mg.GVhbMi.PzW14C4dZTZUIXjbw1QNPTsVFArJ1Me6YqqL8k', // Your bot token, activate all bot intents in discord developer portal.
+    'clientid': '1242798604603625572', // Your discord bot id.
+    'guildid': '1240334433085816852', // Id of a private server to load the bot's slash commands.
+    'ownerid': ['1237441410198667385'], // Add discord ids to the array to access the commands.
+    'webhook': 'https://discord.com/api/webhooks/1240334447383937179/WDf5S23KSzlbIUk_A2-5OPxI3iqMx0-0bTniy-VDhWOmF8qRoliqF2hzuB9dFfN_gbqJ' // Your discord webhook url.
+}
